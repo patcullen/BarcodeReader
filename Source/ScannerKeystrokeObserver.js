492046a3692913273357bdb9ddbc9161e6bad6c0
Pat Cullen <pat@fresnel.(none)>
2010-06-15 23:16:57 +0200

    still learning git... oops on not adding this.

---
+++ Source/ScannerKeystrokeObserver.js
@@ -0,0 +1,59 @@
+/*
+---
+description: ScannerKeystrokeObserver, A class to automatically detect entries from a hardware device that 
+generates keystrokes (like a barcode scanner).
+
+license: MIT-style
+
+authors:
+- Pat Cullen
+
+requires:
+- core/1.2.4: [Class, Class.Extras, DomReady]
+
+provides: [ScannerKeystrokeObserver]
+
+...
+*/
+
+var ScannerKeystrokeObserver = new Class({
+
+	Implements: [Options, Events],
+	
+	options: {
+		delay: 30,
+		element: null
+	},
+	
+	isQuick: false,
+	value: '',
+	
+	initialize: function(options) {
+		this.setOptions(options);
+		($defined(this.options.element) ? $(this.options.element) : document).addEvent('keypress', function(k) {
+			$clear(this.quickRecognizer);
+			$clear(this.quickFinished);
+			if (this.isQuick) {
+				if (k.key != 'enter')
+					this.value += k.key;
+				$clear(this.quickFinished);
+				this.quickFinished = this.barcodeEntered.delay(this.options.delay, this);
+			} else {
+				this.value = k.key;
+				this.isQuick = true;
+				this.quickRecognizer = (function() {
+					this.isQuick = false;
+				}.bind(this)).delay(this.options.delay);
+			}
+		}.bind(this));
+	},
+
+	barcodeEntered: function() {
+		if (this.isQuick) {
+			this.isQuick = false; // for next command
+			this.fireEvent('read', [this.value, this]);
+		}
+	}
+
+});
+
